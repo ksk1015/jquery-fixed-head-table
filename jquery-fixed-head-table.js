2c1b9727b8d59a58ad2ffe47ddadfcf269a2e5c8
ksk1015 <sizznami@gmail.com>
2016-05-27 16:35:56 +0900

    自動更新を修正

--- jquery-fixed-head-table.js
+++ jquery-fixed-head-table.js
@@ -13,47 +13,75 @@
 		};
 	}
 
-	$.fn.fixedHeadTable = function(height){
+	function init(el, height, killAutoUpdate){
+		var $el = $(el).css('overflow', 'hidden');
+		if ( height ) $el.height(height);
+
+		var $table = $el.children('table').css({
+			boxSizing: 'border-box',
+			margin: '0',
+			width: '100%'
+		});
+
+		var $thead = $table.find('thead');
+
+		var $head = $('<div class="fixed-head-table-head">').css('overflow', 'hidden').append($table.clone()).prependTo($el);
+
+		$table.wrap('<div class="fixed-head-table-body"><div class="fixed-head-table-body-inner"></div></div>');
+		var $bodyInner = $table.parent().css('overflow', 'hidden');
+		var $body = $bodyInner.parent().css('overflowY', 'scroll');
+
+		var bodyWidth, bodyInnerWidth, headHeight, bodyHeight;
+
+		function update(){
+			var _bodyWidth = $body.width();
+			var _bodyInnerWidth = $bodyInner.width();
+			var _headHeight = $.css($table.get(0), 'borderTopWidth', true) + $thead.outerHeight() + $.css($table.find('tbody > tr:first-child > *:first-child').get(0), 'borderTopWidth', true);
+			var _bodyHeight = $el.height() - _headHeight;
+
+			if (_bodyWidth === bodyWidth &&
+				_bodyInnerWidth === bodyInnerWidth &&
+				_headHeight === headHeight &&
+				_bodyHeight === bodyHeight ) {
+				return;
+			}
+			bodyWidth = _bodyWidth;
+			bodyInnerWidth = _bodyInnerWidth;
+			headHeight = _headHeight;
+			bodyHeight = _bodyHeight;
+
+			$head.css({
+				width: bodyWidth === bodyInnerWidth ? '' : bodyInnerWidth + 'px',
+				height: headHeight + 'px'
+			});
+			$body.css({
+				height: bodyHeight + 'px'
+			});
+			$bodyInner.css({
+				marginTop: - headHeight + 'px'
+			});
+		}
+
+		update();
+
+		if ( !killAutoUpdate ) {
+			$(window).on('resize', myThrottle(250, update));
+			setInterval(update, 1000);
+		}
+
+		$el.data('fixedHeadTable-update', update);
+	}
+
+	$.fn.fixedHeadTable = function(height, killAutoUpdate){
 
 		return this.each(function(){
 
-			var $el = $(this).css('overflow', 'hidden');
-			if ( height ) $el.height(height);
-
-			var $table = $el.children('table').css({
-				boxSizing: 'border-box',
-				margin: '0',
-				width: '100%'
-			});
-
-			var $thead = $table.find('thead');
-
-			var $head = $('<div class="fixed-head-table-head">').css('overflow', 'hidden').append($table.clone()).prependTo($el);
-
-			$table.wrap('<div class="fixed-head-table-body"><div class="fixed-head-table-body-inner"></div></div>');
-			var $bodyInner = $table.parent().css('overflow', 'hidden');
-			var $body = $bodyInner.parent().css('overflowY', 'scroll');
-
-			update();
-			$(window).on('resize', myThrottle(250, update));
-
-			function update(){
-				var bodyWidth = $body.width();
-				var bodyInnerWidth = $bodyInner.width();
-				var headHeight = $.css($table.get(0), 'borderTopWidth', true) + $thead.outerHeight() + $.css($table.find('tbody > tr:first-child > *:first-child').get(0), 'borderTopWidth', true);
-				var bodyHeight = $el.height() - headHeight;
-				console.log(headHeight);
-
-				$head.css({
-					width: bodyWidth === bodyInnerWidth ? '' : bodyInnerWidth + 'px',
-					height: headHeight + 'px'
-				});
-				$body.css({
-					height: bodyHeight + 'px'
-				});
-				$bodyInner.css({
-					marginTop: - headHeight + 'px'
-				});
+			if ( height === 'update' ) {
+				if ( $(this).data('fixedHeadTable-update') ) {
+					$(this).data('fixedHeadTable-update')();
+				}
+			} else {
+				init(this, height, !!killAutoUpdate);
 			}
 
 		});
